0bed1888c15ae1a882ca88090e4014fb37d759d0
alizscripts <ah4855132@gmail.com>
2026-08-18 22:33:29 +0330

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/space-explore-site/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
